eb88e811748796e5c2c739e7de0f2db6f9d602ff
Z Naddaf <znaddaf@protonmail.com>
2020-01-21 17:04:34 -0600

    Added initial libraries and expanded questions

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 //======================================================================
 const fs = require("fs");
 const inquirer = require("inquirer");
-const axios = require("axios");
 
 //======================================================================
 // Inquirer Questions
@@ -13,52 +12,74 @@ inquirer
     .prompt([
         ////////////// Start by prompting for user role
         {
+            type: "input",
+            name: "name",
+            message: "What is your name?"
+        },
+        {
+            type: "input",
+            name: "employeeId",
+            message: "What is your employee ID?"
+        },
+        {
             type: "list",
             message: "What is your company role?",
             name: "role",
             choices: [
-                "employee",
                 "engineer",
                 "intern",
                 "manager"
             ]
         },
 
-        ////////////// if engineer
-        {
-            type: "input",
-            name: "username",
-            message: "Please enter your GitHub Username"
-        },
+        ////////////// Employee Information
+    ]).then(
+        function (answer) {
+            roleQuestions(answer.role)
+        }
+    )
 
-        ////////////// if intern
-        {
-            type: "input",
-            name: "school",
-            message: "What school do you attend?"
-        },
+function roleQuestions(role) {
+    console.log("role: ", role)
+    const questions = [];
+    if (role === "engineer") {
+        questions.push(
+            {
+                type: "input",
+                name: "username",
+                message: "Please enter your GitHub Username"
+            }
+        )
+    }
+    inquirer
+        .prompt(
 
-        ////////////// if manager
-        {
-            type: "input",
-            name: "officenumber",
-            message: "What is your office number?"
-        },
-        {
-            type: "input",
-            name: "yacht",
-            message: "How big is your very impressive yacht?"
-        },
+            ////////////// if engineer
+            questions
+            ////////////// if intern
+            // {
+            //     type: "input",
+            //     name: "school",
+            //     message: "What school do you attend?"
+            // },
 
-    ])
+            // ////////////// if manager
+            // {
+            //     type: "input",
+            //     name: "officenumber",
+            //     message: "What is your office number?"
+            // },
+            // {
+            //     type: "input",
+            //     name: "yacht",
+            //     message: "How big is your very impressive yacht?"
+            // },
 
+        )
+}
 
 
 //======================================================================
 // HTML Output
 //======================================================================
 
-
-//======================================================================
-// Unit Tests
-//======================================================================
